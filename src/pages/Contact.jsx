0fa7321107ab612e95c08a9edce5c721785fae9f
Oveshek  Kundu Toton <oveshekkundu@gmail.com>
2026-06-29 21:47:02 +0600

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -58,7 +58,7 @@ const Contact = () => {
 
               <div className={`${styles.infoBlock} reveal`}>
                 <h3>Address</h3>
-                <p>Kazi Para, Second Road<br />Jashore, Bangladesh</p>
+                <p>Kazi Para, Second Road<br />Dhaka, Bangladesh</p>
               </div>
 
               <div className={`${styles.infoBlock} reveal`}>
